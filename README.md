35a7ff31d082b3d63f490478d57385a4fb262d16
Gareth Beddis <garethbeddis@gmail.com>
2019-07-11 20:05:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# garethbeddis.com
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: './src/app/index',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: './'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+}
